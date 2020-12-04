3ead3724ad6026fb6220b1421ed072079c8977d5
prashanthtr <prashanth.thattai@gmail.com>
2020-12-04 16:04:52 +0800

    added drip texture with continuous sweeps.

--- myPopPatternSynth.py
+++ myPopPatternSynth.py
@@ -2,25 +2,25 @@ import numpy as np
 import math
 
 import synthInterface as SI
-from myPop import MyPop  # This is "event" synthesizer this pattern synth will use
+from myDrip import MyDrip  # This is "event" synthesizer this pattern synth will use
 
 ################################################################################################################
 class MyPopPatternSynth(SI.MySoundModel) :
 
-	def __init__(self, cf=440, Q=10, rate_exp=0, irreg_exp=1) :
+	def __init__(self, f0=440, f1=880, rate_exp=0, irreg_exp=1) :
 
                 SI.MySoundModel.__init__(self)
 		#create a dictionary of the parameters this synth will use
-                self.__addParam__("cf", 100, 2000, cf,
+                self.__addParam__("f0", 100, 2000, f0,
 			lambda v :
 				self.evSynth.setParam('f0', v))
-                self.__addParam__("Q", .1, 50, Q,
+                self.__addParam__("f1", 100, 2000, f1,
 			lambda v :
-                                self.evSynth.setParam('Q', v))
+                                self.evSynth.setParam('f1', v))
                 self.__addParam__("rate_exp", -10, 10, rate_exp)
                 self.__addParam__("irreg_exp", .1, 50, irreg_exp)
 
-                self.evSynth=MyPop(cf, Q)
+                self.evSynth=MyDrip(f0,f1)
 
 	'''
 		Override of base model method
@@ -38,9 +38,11 @@ class MyPopPatternSynth(SI.MySoundModel) :
                         startsamp=int(round(nf*self.sr))%numSamples
                         # create some deviation in center frequency
                         cfsd = 1
-                        perturbedCf = self.getParam("cf")*np.power(2,np.random.normal(scale=cfsd)/12)
+                        perturbedf0 = self.getParam("f0")*np.power(2,np.random.normal(scale=cfsd)/12)
+                        perturbedf1 = self.getParam("f1")*np.power(2,np.random.normal(scale=cfsd)/12)
 
-                        self.evSynth.setParam("f0", perturbedCf)
+                        self.evSynth.setParam("f0", perturbedf0)
+                        self.evSynth.setParam("f1", perturbedf1)
                         sig = SI.addin(self.evSynth.generate(1), sig, startsamp)
 
                 return sig
